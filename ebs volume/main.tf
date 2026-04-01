9f559cb8438fc41aee72036d9f76ce4982d11a25
servsanaws <servsanuspro@gmail.com>
2026-04-01 23:02:13 +0530

    ebs volume updated with new resource for ebs

--- ebs volume/main.tf
+++ ebs volume/main.tf
@@ -1,15 +1,11 @@
 # ebs volume creation //
 
-resource "aws_ebs_volume" "xfusion" {
-    availability_zone = "us-east-1"
-    size              = 2
-    type       = "gp3"
-
-    tags = {
-    Name = "xfusion-volume"
-}
+resource "aws_ebs_volume" "nautilus-volume" {  
+  
+  availability_zone = "us-east-1a"
+  size              = 2
+  type              = "gp3"
 }
 
-output "volume" {   
-  value = aws_ebs_volume.xfusion.id
-}
+
+
